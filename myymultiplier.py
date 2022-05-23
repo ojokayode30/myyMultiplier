33b39e8c542580735e6c6bcf04669e372865fd2f
ojokayode30 <ojokayode30@gmail.com>
2022-05-24 03:38:34 +0800

    myymultiplier

---
+++ myymultiplier.py
@@ -0,0 +1,10 @@
+i=1    
+number=0    
+b=9    
+number = int(input("Enter the number:"))    
+while i<=10:    
+    print("%d X %d = %d \n"%(number,i,number*i))    
+    i = i+1 
+print("===================================================")
+print("Thanks for using our multiplier")
+print("===================================================")
